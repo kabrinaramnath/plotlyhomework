ac8e7e7d13907233137c8723b562578f1f8e8bb5
Kabrina Ramnath <kabrinar@kabrinas-mbp.attlocal.net>
2019-03-29 20:46:51 -0500

    update code

--- StarterCode/Belly_Button_Biodiversity/static/js/app.js
+++ StarterCode/Belly_Button_Biodiversity/static/js/app.js
@@ -3,15 +3,21 @@ function buildMetadata(sample) {
   // @TODO: Complete the following function that builds the metadata panel
 
   // Use `d3.json` to fetch the metadata for a sample
+
   var metadataURL = `/metadata/${sample}`;
+
     // Use d3 to select the panel with id of `#sample-metadata`
+
     d3.json(metadataURL).then(function(sample){
       var sampleData = d3.select(`#sample-metadata`);
+
     // Use `.html("") to clear any existing metadata
       sampleData.html("");
+
     // Use `Object.entries` to add each key and value pair to the panel
     // Hint: Inside the loop, you will need to use d3 to append new
     // tags for each key-value in the metadata.
+
       Object.entries(sample).forEach(function([key,value]){
         var row = sampleData.append("p");
         row.text(`${key}:${value}`)
@@ -19,10 +25,16 @@ function buildMetadata(sample) {
     });
 }
 
-function buildCharts(sample) {
+
   // @TODO: Use `d3.json` to fetch the sample data for the plots
-  var plotData = `/samples/${sample}`;
   // @TODO: Build a Bubble Chart using the sample data
+
+
+function buildCharts(sample) {
+
+  var plotData = `/samples/${sample}`;
+
+
   d3.json(plotData).then(function(data){
     var x_axis = data.otu_ids;
     var y_axis = data.sample_values;
@@ -49,6 +61,7 @@ function buildCharts(sample) {
     Plotly.newPlot("bubble", data, layout);
 
     // @TODO: Build a Pie Chart
+
     d3.json(plotData).then(function(data){
       var values = data.sample_values.slice(0,10);
       var labels = data.otu_ids.slice(0,10);
@@ -70,10 +83,13 @@ function buildCharts(sample) {
 
 function init() {
   console.log('hello');
+
   // Grab a reference to the dropdown select element
+
   var selector = d3.select("#selDataset");
 
   // Use the list of sample names to populate the select options
+
   d3.json("/names").then((sampleNames) => {
     sampleNames.forEach((sample) => {
       selector
@@ -83,6 +99,7 @@ function init() {
     });
 
     // Use the first sample from the list to build the initial plots
+
     const firstSample = sampleNames[0];
     buildCharts(firstSample);
     buildMetadata(firstSample);
@@ -90,14 +107,14 @@ function init() {
 }
 
 function optionChanged(newSample) {
+
   // Fetch new data each time a new sample is selected
+
   buildCharts(newSample);
   buildMetadata(newSample);
 }
 
 // Initialize the dashboard
-<<<<<<< HEAD
+
 init();
-=======
-init();
->>>>>>> f162c71214c54b446709c42094d48f0c8a9af143
+
